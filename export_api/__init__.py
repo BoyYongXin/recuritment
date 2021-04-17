cd136ef67842ac211720d1e64c04817b5762d9a2
yangyongxin <yangyongxin@yidian-inc.com>
2021-04-17 20:14:39 +0800

    add:实现 简单的注册

--- export_api/__init__.py
+++ export_api/__init__.py
@@ -4,4 +4,6 @@
 #查看职位详情
 # http://127.0.0.1:8000/job/1/
 
+# python3 manage.py runserver --settings=settings.local
+
 
---
+++ settings/production.py
@@ -0,0 +1,4 @@
+import os
+from .base import *
+
+ALLOWED_HOSTS = ["127.0.0.1"]
