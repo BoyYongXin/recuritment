c4fa9bc16e23b6aaa395a8fc6864f500c6fddc12
yangyongxin <yangyongxin@yidian-inc.com>
2021-05-10 14:30:11 +0800

    update: 完成在线提交简历申请职位功能

--- jobs/urls.py
+++ jobs/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     # path("joblist/", views.joblist, name="joblist"),
     url(r"^joblist/",views.joblist, name="joblist"),
     url(r"^job/(?P<job_id>\d+)/$",views.detail, name="detail"),
+    # 申请职位
+    path('resume/add/', views.ResumeCreateView.as_view(), name='resume-add'),
     # 首页自动跳转到 职位列表
     # url(r"^$", views.joblist, name="name"),
     path("", views.joblist, name="name"),
--- jobs/views.py
+++ jobs/views.py
@@ -7,6 +7,10 @@ from jobs.models import Cities, JobTypes
 from django.template import RequestContext, loader
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin # 继承多个类
+from django.views.generic.edit import CreateView
+from jobs.models import Resume
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -22,6 +26,11 @@ logger = logging.getLogger(__name__)
 #     return HttpResponse(template.render(context))
 
 def joblist(request):
+    """
+     职位详情列表页面
+    :param request:
+    :return:
+    """
     job_list = Job.objects.order_by('job_type')
     context =  {'job_list': job_list}
     for job in job_list:
@@ -30,6 +39,13 @@ def joblist(request):
     return render(request, 'joblist.html', context)
 
 def detail(request, job_id):
+    """
+    职位详情页面
+    :param request:
+    :param job_id:
+    :return:
+    """
+
     try:
         job = Job.objects.get(pk=job_id)
         job.city_name = Cities[job.job_city][1]
@@ -37,3 +53,31 @@ def detail(request, job_id):
     except Job.DoesNotExist:
         raise Http404("Job does not exist")
     return render(request, 'job.html', {'job': job})
+
+class ResumeCreateView(LoginRequiredMixin, CreateView):
+    """    简历职位页面  """
+    template_name = 'resume_form.html'
+    success_url = '/joblist/'
+    model = Resume
+    fields = ["username", "city", "phone",
+        "email", "apply_position", "gender",
+        "bachelor_school", "master_school", "major", "degree", "picture", "attachment",
+        "candidate_introduction", "work_experience", "project_experience"]
+
+    ### 从 URL 请求参数带入默认值
+    def get_initial(self):
+        initial = {}
+        for x in self.request.GET:
+            initial[x] = self.request.GET[x]
+        return initial
+
+    def form_valid(self, form):
+        """
+        验证表单
+        :param form:
+        :return:
+        """
+        self.object = form.save(commit=False)
+        self.object.applicant = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
